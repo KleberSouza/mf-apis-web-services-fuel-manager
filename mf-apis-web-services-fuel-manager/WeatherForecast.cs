ccf2543ebdfada1e5c389d94acef74c164cc128b
Kleber Souza <kleberjfsouza@gmail.com>
2022-06-10 11:47:14 -0300

    Adicionar arquivos de projeto.

---
+++ mf-apis-web-services-fuel-manager/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace mf_apis_web_services_fuel_manager
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
