e0a687ddb9a3986d60aea8b6b928b1d7b3082429
Kleber Souza <kleberjfsouza@gmail.com>
2022-06-10 12:17:10 -0300

    Criação e Configuração dos modelos de Veículo e Consumo

---
+++ mf-apis-web-services-fuel-manager/Models/Consumo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+
+        [Required]
+        public int VeiculoId { get; set; }
+
+        public Veiculo Veiculo { get; set; }
+    }
+
+    public enum TipoCombustivel
+    {
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
---
+++ mf-apis-web-services-fuel-manager/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Placa { get; set; }
+        [Required]
+        public int AnoFabricacao { get; set; }
+        [Required]
+        public int AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
